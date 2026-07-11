d906f9dec1c90ec4d955659a18261a18d13d817c
Voxelix-7 <ayatrshaalan15@gmail.com>
2026-07-11 18:32:22 +0300

    Update game.js

--- public/game.js
+++ public/game.js
@@ -5,7 +5,7 @@ const CONFIG = {
   gravity: 0.6,
   jump: -11,
   playerSpeed: 3.4,
-  catBaseSpeed: 2.6,
+  catBaseSpeed: 3.1,
   catGain: 0.00012, // px/frame^2, ramps up over time
   levelLength: 5400,
   groundY: 300,
