a77f512884fd9d74fa77b236092192f8fdfd2e86
Voxelix-7 <ayatrshaalan15@gmail.com>
2026-07-24 16:38:29 +0300

    Update game.js

--- public/game.js
+++ public/game.js
@@ -161,10 +161,10 @@ export async function startGame({ canvas, progressEl, onLose, onWin, startPaused
     if (!done) {
       // cat catch
       if (rectHit(player, cat)) return finish(false);
-      // enemies
-      for (const e of enemies) {
-        if (rectHit(player, e)) return finish(false);
-      }
+      // enemies TEMPORARILY DISABLED
+      //for (const e of enemies) {
+        //if (rectHit(player, e)) return finish(false);
+      //}
       // target (cake, envelope, chess piece, or cd)
       if (player.x + player.w > targetX) return finish(true);
     }
